c4fc332eac09c39e535b9522ff326a6abb466c42
edward sharp <edward@edwardsharp.net>
2018-08-05 17:24:02 -0400

    tidy main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
+import './assets/reset.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
 import 'material-design-icons/iconfont/material-icons.css'
-import './assets/reset.css'
-
 import { MdButton, MdMenu, MdList } from 'vue-material/dist/components'
-// import VueMaterial from 'vue-material'
-
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/black-green-light.css' // This line here
 
-// Vue.use(VueMaterial)
 Vue.use(MdButton)
 Vue.use(MdMenu)
 Vue.use(MdList)
