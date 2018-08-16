8381cf319b4dd27d21c198043428fded339a41e3
edward sharp <edward@edwardsharp.net>
2018-08-16 04:07:32 -0400

    tidy accordion

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,40 +1,34 @@
 <template>
-  <article class="message" :class="accordionClasses">
-    <div @click="toggleAccordion" id="heading">
+  <article :class="accordionClasses">
+    <div @click="toggleAccordion" class="heading">
       <i class="material-icons" v-if="isOpen">keyboard_arrow_up</i>
       <i class="material-icons" v-else>keyboard_arrow_down</i>
       <slot name="header"></slot>
     </div>
-    <div id="body">
-      <div id="content">
+    <div class="body">
+      <div class="content">
         <slot></slot>
       </div>
     </div>
   </article>
 </template>
 <style scoped>
-.message {
-  max-width: 500px;
-  margin-left: auto;
-  margin-right: auto;
-}
-#heading {
+.heading {
   cursor: pointer;
   display: flex;
   align-items: center;
   color: #88170e;
 }
-
-#body   {
+.body   {
   padding: 0;
   max-height: 10em;
   overflow: hidden;
-  transition: 0.3s ease all;
+  transition: 0.5s ease all;
 }
-.is-closed #body {
+.is-closed .body {
   max-height: 0;
 }
-#content {
+.content {
   padding: 20px;
 }
 </style>
