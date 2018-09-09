f3b5c8cf9933b57926e478588fc459f8107427a5
edward sharp <edward@edwardsharp.net>
2018-09-09 19:42:43 -0400

    make geoJSON minify script sub-directory recursive

--- util/minify_dist_geojson.js
+++ util/minify_dist_geojson.js
@@ -6,16 +6,39 @@ const path = require('path');
 
 const dir = './dist/geojson/';
 
-fs.readdir(dir, function(err, filez) {
-  if (err) throw err;
-  filez.forEach(function(f) {
-    fs.readFile(path.resolve(dir, f), 'utf-8', function(err, content) {
-      if (err) throw err;
-      const data = JSON.parse( fs.readFileSync(path.resolve(dir, f)) );
-      fs.writeFile(path.resolve(dir, f), JSON.stringify(data), (err) => {  
-          if (err) throw err;
-          console.info('minified:', f);
-      });
-    });
+function readDir(_dir) {
+  fs.readdir(_dir, function(err, filez) {
+    if (err){ 
+      console.warn('0noz! caught error! dir:', _dir, ' err:', err);
+    } else {
+      readFilez(filez, _dir);
+    }
   });
-});
+}
+  
+function readFilez(filez, _dir) {
+  filez.forEach(function(f) {
+    const filePath = path.resolve(_dir, f);
+    if (fs.lstatSync(filePath).isDirectory()) {
+      readDir(filePath);
+    } else { 
+      fs.readFile(filePath, 'utf-8', function(err, content) {
+        if (err){ 
+          console.warn('1noz! caught error! dir:', _dir, ' f:', f, 'err:', err);
+        } else {
+          const data = JSON.parse( fs.readFileSync(filePath) );
+          fs.writeFile(filePath, JSON.stringify(data), (err) => {  
+              if (err){ 
+                console.warn('2noz! caught error! ..bad .geoJSON data? dir:', _dir, ' err:', err);
+              } else {
+                console.info('minified:', filePath);
+              }
+          });
+        }
+      });
+    }
+  });
+}
+
+// INIT
+readDir(dir);
