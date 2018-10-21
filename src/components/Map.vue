d56558aa525e5a06bba9dce170696ebe45ac61da
edward sharp <edward@edwardsharp.net>
2018-10-21 02:45:56 -0400

    fix vimeo overlay #9

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -205,7 +205,7 @@ export default {
       this.$refs.popupCloser.blur()
       this.twitterPopup.setPosition(undefined)
       this.$refs.twitterpopupCloser.blur()
-      this.$refs.vimeoPopup.setPosition(undefined)
+      this.vimeoPopup.setPosition(undefined)
       this.$refs.vimeoPopupCloser.blur()
       return false
     },
