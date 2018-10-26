0f01a33a3d0d8005c4629ed8e12ed2ce4178ee06
Brian <bhcontinentaldrift@gmail.com>
2018-10-25 23:20:53 -0500

    Salmon details

--- src/views/bioregion/BioregionIntroduction.vue
+++ src/views/bioregion/BioregionIntroduction.vue
@@ -12,24 +12,24 @@ text-align: justify;
 
 <template>
   <div class="aside-content">
-    <div class="title"><h1>Bioregional turn</h1></div>
+    <div class="title"><h1>The old-new idea</h1></div>
     <img src="images/Cascade.jpg" class="fullwidth">
     <br>
     <div class="caption">Falling water, Olympic Peninsula</div>
     <br>
     <Accordion :open="false">
-      <h3 slot="header">Work in progress!</h3>
+      <h3 slot="header">Legends</h3>
       <div class="flex">
-        <p>Check back after November 1 and the whole concept will be a lot clearer...</p>
+        <p></p>
       </div>
     </Accordion>
     <br>
 
 <p class="quote">"Living-in-place means following the necessities and pleasures of life as they are uniquely presented by a particular site.... Reinhabitation means learning to live-in-place in an area that has been disrupted and injured through past exploitation."</p>
 
-<p>With these words, Peter Berg and Raymond Dasmann launched bioregionalism. They conceived it not as starting fresh but as returning to things forgotten, with an awareness of past mistakes and a particular respect for the earliest inhabitants of North America. The time was forty years ago. The inspiration was poetic, scientific and intensely political.</p>
+<p>With these ideas, Peter Berg and Raymond Dasmann invented bioregionalism. They conceived it not as starting fresh but as returning to things forgotten, with an awareness of past mistakes and a particular respect for the earliest inhabitants of North America. That was forty years ago. The inspiration was poetic, scientific and political.</p>
 
-<p>Today, the Cascadia bioregion is a vast and diffuse idea that calls attention to the entangled destinies of natural environments and human beings. Thanks largely to the cartography of <a href="http://cascadia-institute.org" target="_blank" rel="nofollow">David McCloskey</a>, the outline of Cascadia is recognized across national boundaries. Some movements call for its independence. For others, interdependence is the crucial thing.</p>
+<p>Today, the Cascadia bioregion is a vast and diffuse idea that calls attention to the entangled destinies of natural environments and human beings. Thanks to the cartography of <a href="http://cascadia-institute.org" target="_blank" rel="nofollow">David McCloskey</a>, the outline of Cascadia is recognized across national boundaries. Some movements call for its independence. For others, interdependence is the crucial thing.</p>
 
 <p>It's clear that the most powerful contributions to Cascadia come from indigenous peoples. Their assertion of traditional practices brings them to negotiating tables where the fate of land and water is decided. Often they invoke the needs of non-human species. Listen to the words of Native American activist <a href="http://billyfrankjr.org" target="_blank" rel="nofollow">Billy Frank Jr.</a>, who lived along the banks of the Nisqually River:</p>
 
@@ -46,10 +46,35 @@ text-align: justify;
 
 </div>
 </template>
-
+<style scoped>
+.flex {
+  display: flex;
+  flex-direction: column;
+}
+.flex div {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5em;
+}
+.flex img {
+  margin-right: 0.5em;
+}
+</style>
 <script>
+import AppLightBox from '../../components/AppLightBox.vue'
+import Accordion from '../../components/Accordion.vue'
+import {eventBus} from '../../main'
 
 export default {
-  name: 'BioregionIntroduction'
+  name: 'BioregionIntro',
+  components: {
+    Accordion,
+    AppLightBox
+  },
+  methods: {
+    emit: function (method, args) {
+      eventBus.$emit(method, args)
+    }
+  }
 }
 </script>
