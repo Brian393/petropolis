f72c5ff991816a22602af656eb3009cb213185e6
Brian393 <yobhcontinentaldrift@gmail.com>
2019-03-16 14:16:10 -0500

    Correct legends in Dancing

--- src/views/bioregion/BioregionDancing.vue
+++ src/views/bioregion/BioregionDancing.vue
@@ -35,7 +35,7 @@ color: #333333; font-style: italic; text-align: center;
     <Accordion>
       <h3 slot="header">legends</h3>
       <div class="flex">
-        <p>This page records the artist's travels from January 13 to Nov 1, 2018 (opening day of "The Earth Will Not Abide," featuring <em>Cache VII</em>). Each circle represents a journey devoted to the development of her practice.</p>
+        <p>This page records Sara Siestreem's travels in 2018, under the title "Seasonal Round." Each circle represents a journey devoted to the development of her practice.</p>
       </div>
     </Accordion>
     <br>
