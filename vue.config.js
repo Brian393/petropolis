d55992eafaf3f1df00acea32acf8c33f945f43b7
Brian <bhcontinentaldrift@gmail.com>
2020-04-22 19:12:15 -0500

    Merge branch 'feat/info-pop-up' into P4

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 //  baseUrl: '/',
 //  productionSourceMap: false,
+  runtimeCompiler: true, // https://codewithhugo.com/vue-warn-runtime-only-build/
   chainWebpack: config => { // https://github.com/vuejs/vue-cli/issues/1669
     config.plugin('html').tap(args => {
       args[0].chunksSortMode = 'none'
